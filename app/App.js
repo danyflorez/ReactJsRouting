2968e625925d610469120226c72858d0700d4549
Daniel Florez Jaramillo <Daniel Florez Jaramillo>
2016-09-06 07:03:21 -0500

    second commit

--- app/App.js
+++ app/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+
+// first we import some components
+import { Router, Route, Link , IndexRoute} from 'react-router';
+
+import About from './About';
+import Home from './Home';
+import Repos from './Repos';
 
 class App extends Component {
-  render(){
+  render() {
     return (
-      <h1>Hello World</h1>
+      <div>
+        <header>App</header>
+        <menu>
+          <ul>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/repos">Repos</Link></li>
+          </ul>
+        </menu>
+        {this.props.children}
+      </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render((
+  <Router>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="about" component={About}/>
+      <Route path="repos" component={Repos}/>
+    </Route>
+  </Router>
+), document.getElementById('root'));
